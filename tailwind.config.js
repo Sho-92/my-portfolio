0a3ac287ef10fa95a44816d600095414cfcd2558
Sho Maekawa <s.maekawa0509@gmail.com>
2024-10-24 18:32:01 +0900

    Fixed package.json for pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './src/**/*.{html,js}',  // src内のHTMLおよびJSファイルを監視
-    './dist/**/*.html'        // dist内のHTMLファイルを監視
+    './docs/**/*.html'        // docs内のHTMLファイルを監視
   ],
   theme: {
     extend: {},
